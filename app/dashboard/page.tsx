6bd3d80803c2d875eee9c25200bdcd82a60ac676
xInr0u <xinrou1961@gmail.com>
2025-08-02 17:43:27 +0800

    dashboard responsive

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,7 +6,7 @@ import Header from "@/components/custom/Header";
 export default function DashboardPage() {
   return (
     <main className="min-h-screen flex flex-col">
-      <div className="flex flex-col pt-4 pb-2 flex-1 items-center px-6 justify-between">
+      <div className="flex flex-col pt-4 pb-2 px-4 sm:px-6 md:px-8 lg:px-10 flex-1 items-center justify-between">
         <Header />
         <ChatSection></ChatSection>
       </div>
--- components/custom/Header.tsx
+++ components/custom/Header.tsx
@@ -6,8 +6,8 @@ import { IoMdMenu } from "react-icons/io";
 
 export default function Header() {
   return (
-    <header className="flex justify-between w-full items-center">
-      <div>
+    <header className="flex items-center justify-between w-full flex-wrap gap-4 px-2 sm:px-4 md:px-0">
+      <div className="flex items-center gap-2">
         <IoMdMenu size={24} />
       </div>
       <div className="relative w-[180px] lg:w-[240px] h-[72px] lg:h-[96px]">
